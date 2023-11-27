44233d8a6f5cf11209e1952353b54d9aa413163f
Alexander Petrov <alexonrails@gmail.com>
2023-11-27 13:05:28 +0100

    wip

--- test/csv_pyawk.py
+++ test/csv_pyawk.py
@@ -1,5 +1,13 @@
-
+import pandas as pd
+acc = []
 def many(xs):
-    data = [ t.split("\t") for t in xs]
+    data = [ t.strip().split("\t") for t in xs]
     print(len(data))
+    acc.append(pd.DataFrame(data))
+
+def end():
+    print(pd.concat(acc))
+
+
+
 
