dea4c0c8d6738c635df346661df1c43e7eea9f4a
Alexander Petrov <alexonrails@gmail.com>
2023-11-28 19:24:08 +0100

    update test/csv_pyawk.py example

--- test/csv_pyawk.py
+++ test/csv_pyawk.py
@@ -1,12 +1,16 @@
+import io
+import csv
 import pandas as pd
+
 acc = []
 def many(xs):
-    data = [ t.strip().split("\t") for t in xs]
-    print(len(data))
+    def parse_line(x):
+        return next(csv.reader(io.StringIO(x)))
+    data = [ parse_line(t) for t in xs]
     acc.append(pd.DataFrame(data))
 
 def end():
-    print(pd.concat(acc))
+    print(pd.concat(acc).shape)
 
 
 
